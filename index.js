84b8a3a1e48ca7fb2ad6cbf5c013074303049758
LeahNelson <webdevleah@gmail.com>
2018-03-29 11:41:01 -0500

    updave ENV Var

--- index.js
+++ index.js
@@ -5,25 +5,25 @@
 // Require dependencies
 // _______________________
 var express = require('express'),
-    app     = express(),
-    exphbs  = require('express-handlebars'),
+    app = express(),
+    exphbs = require('express-handlebars'),
     bodyParser = require('body-parser'),
     session = require('express-session'),
-    dotenv = require('dotenv').config();
-    
+    dotenv = require('dotenv').config() || process.env.NODE_ENV;
+
 
 
 // Configuration
 // -------------
 app.engine('hbs', exphbs({
-  defaultLayout: 'default',
-  extname: '.hbs',
-  layoutsDir: __dirname + '/src/views/layouts/',
-  partialsDir: __dirname + '/src/views/partials/'
+    defaultLayout: 'default',
+    extname: '.hbs',
+    layoutsDir: __dirname + '/src/views/layouts/',
+    partialsDir: __dirname + '/src/views/partials/'
 }));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/src/views');
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Connect to database
 // -------------------
@@ -33,10 +33,10 @@ require('./src/config/db');
 // Configure session middleware
 // ----------------------------
 app.use(session({
-  name: 'usersession',
-  resave: false,
-  saveUninitialized: false,
-  secret: 'dbfcebhfqie'
+    name: 'usersession',
+    resave: false,
+    saveUninitialized: false,
+    secret: 'dbfcebhfqie'
 }))
 
 // Middleware
@@ -45,11 +45,11 @@ app.use(express.static(__dirname + '/src/public')); // Serve static files
 app.use('/userregistration', require('./src/controllers/userregistration'));
 app.use(require('./src/controllers/home'));
 app.use('/?', function(req, res, nest) {
-  if(req.session.isLoggedIn === true) {
-    return next();
-  }else{
-    res.redirect('/?')
-  }
+    if (req.session.isLoggedIn === true) {
+        return next();
+    } else {
+        res.redirect('/?')
+    }
 })
 
 
@@ -58,5 +58,5 @@ app.use('/?', function(req, res, nest) {
 // Start the server
 // ----------------
 var server = app.listen(3000, function() {
-  console.log('App is running');
+    console.log('App is running');
 });
