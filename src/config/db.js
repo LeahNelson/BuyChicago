3d50be420e7513f3429dc7995b1b9661e56c252f
LeahNelson <webdevleah@gmail.com>
2018-05-31 15:10:39 -0500

    update mlab URI

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,17 @@
 var mongoose = require('mongoose');
 
-var connectionString = process.env.NODE_ENV === 'production' ? '  mongodb://leah:12825ln@ds139705.mlab.com:39705/buychicago ' : 'mongodb://localhost/buychicago';
+var connectionString = process.env.NODE_ENV === 'production' ? '  mongodb://Leah-Nelson:12825ln1@ds139705.mlab.com:39705/buychicago ' : 'mongodb://localhost/buychicago';
 
 mongoose.connect(connectionString);
 
 mongoose.connection.on('connected', function() {
-  console.log('mongoose connected to ' + connectionString);
+    console.log('mongoose connected to ' + connectionString);
 });
 
 mongoose.connection.on('error', function(err) {
-  console.log('mongoose connected error ' + err);
+    console.log('mongoose connected error ' + err);
 });
 
 mongoose.connection.on('disconnected', function() {
-  console.log('mongoose disconnected ') ;
+    console.log('mongoose disconnected ');
 });
