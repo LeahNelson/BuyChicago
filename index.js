08ea59dcd5366daa98ad25d1575da7c04d14fe76
LeahNelson <webdevleah@gmail.com>
2018-05-31 15:16:24 -0500

    correct error index.js

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ var express = require('express'),
     app = express(),
     exphbs = require('express-handlebars'),
     bodyParser = require('body-parser'),
-    session = require('express-session'),
-    // dotenv = require('dotenv').config() || process.env.NODE_ENV;
+    session = require('express-session');
+// dotenv = require('dotenv').config() || process.env.NODE_ENV;
 
 
 
-    // Configuration
-    // -------------
-    app.engine('hbs', exphbs({
-        defaultLayout: 'default',
-        extname: '.hbs',
-        layoutsDir: __dirname + '/src/views/layouts/',
-        partialsDir: __dirname + '/src/views/partials/'
-    }));
+// Configuration
+// -------------
+app.engine('hbs', exphbs({
+    defaultLayout: 'default',
+    extname: '.hbs',
+    layoutsDir: __dirname + '/src/views/layouts/',
+    partialsDir: __dirname + '/src/views/partials/'
+}));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/src/views');
 app.use(bodyParser.urlencoded({ extended: true }))
